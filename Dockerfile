71df4bfa5d179efbd5232636ec1ec143b4a1d65e
Oliver Wegner <void1976@gmail.com>
2018-12-17 13:35:11 +0100

    working on Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM nginx
+FROM nginx:1.13-alpine
 
-#COPY nginx.conf /etc/nginx/nginx.conf
-#VOLUME /etc/nginx/nginx.conf
+COPY nginx.conf /etc/nginx/nginx.conf
+COPY html /workspace
--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ docker run -it --rm \
        -p 8080:8081 \
        -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro \
        -v $(pwd)/html:/workspace:Z \
-       nginx
+       nginx:1.13-alpine
